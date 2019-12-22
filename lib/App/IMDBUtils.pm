0572c828badaa6c957f615e0f5c08e6da978400f
perlancar (on pc-home) <perlancar@gmail.com>
2019-12-22 17:04:04 +0700

    Initial commit

---
+++ lib/App/IMDBUtils.pm
@@ -0,0 +1,27 @@
+package App::IMDBUtils;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+#use Log::Any '$log';
+
+our %SPEC;
+
+$SPEC{':package'} = {
+    v => 1.1,
+    summary => 'Utilities related to IMDB (Internet Movie Database)',
+};
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
+
+This distribution includes the following CLI utilities:
+
+# INSERT_EXECS_LIST
